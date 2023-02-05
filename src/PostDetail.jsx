38635a38dbd6474edfb3367f4f59f8bb0b7305a0
Supanut1911 <ynotmela@gmail.com>
2023-02-05 16:04:59 +0700

    basic tanstack query

---
+++ src/PostDetail.jsx
@@ -0,0 +1,52 @@
+import { useMutation } from "react-query";
+
+async function fetchComments(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+  return response.json();
+}
+
+async function deletePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  return response.json();
+}
+
+async function updatePost(postId) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  return response.json();
+}
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  const data = [];
+
+  const delMutation = useMutation((postId) => delMutation(postId));
+
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button
+        onClick={(e) => {
+          delMutation.mutate(post.id);
+        }}
+      >
+        Delete
+      </button>{" "}
+      <button>Update title</button>
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
